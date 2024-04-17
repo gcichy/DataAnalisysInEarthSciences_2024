a456ad8ee79631d3a9d9e5d3ff786ebad1e1083e
maahcin <49162059+maahcin@users.noreply.github.com>
2024-04-17 20:17:58 +0200

    eda and data transformation

--- src/main_script.py
+++ src/main_script.py
@@ -1,21 +1,27 @@
 from data_preparation import get_external_data as ged
 from data_preparation import merge_to_df as mrg
+from data_transformation import get_raw_data as grd
+from data_transformation import transform_data as td
 import time
-
 
 
 def main():
     try:
-        #ged.get_external_data()
-        
-        input_dir = ged.get_input_dir()
-        #ged.break_to_remove_zip_files(input_dir)
-        
-        stations = mrg.subset_stations(input_dir)
-        mrg.merge_all_files(input_dir,stations)
-        
+        # ged.get_external_data()
+
+        #input_dir = ged.get_input_dir()
+        # ged.break_to_remove_zip_files(input_dir)
+
+        #stations = mrg.subset_stations(input_dir)
+        # mrg.merge_all_files(input_dir,stations)
+
+        raw_data_dir = grd.get_raw_data_dir()
+        td.transform_data(raw_data_dir)
+
+
     except Exception as e:
         print('Error:' + str(e))
-        
+
+
 if __name__ == "__main__":
     main()
